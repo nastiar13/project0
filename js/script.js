773920e9834e7a90f8edc183373f042e3752ae71
nastiar13 <jahatmamang0@gmail.com>
2021-09-13 15:45:38 +0700

    initialized

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// event pada saat link di klik
+$('.page-scroll').on('click', function(e){
+    // ambil isi href
+    var href = $(this).attr('href');
+    var elementHref = $(href);
+    
+    $('html,body').animate({
+        scrollTop: elementHref.offset().top,
+    },1250,'easeInOutExpo')
+
+    e.preventDefault();
+});
