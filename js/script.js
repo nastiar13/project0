c5a85f712d43d9c54d92ffa75618df04279afc47
nastiar13 <jahatmamang0@gmail.com>
2021-09-16 09:27:23 +0700

    edit dikit

--- js/script.js
+++ js/script.js
@@ -11,19 +11,45 @@ $('.page-scroll').on('click', function(e){
     e.preventDefault();
 });
 
-
+// landing page
+$(window).on('load',function(){
+    setTimeout(function(){
+        $('.about').addClass('muncul');
+    },500)
+});
 // paralax ==================================================
 
 $(window).scroll(function() {
     let wScroll = $(this).scrollTop();
-
+    // about
+    $('.about .container').css({
+        'transform': 'translate(0px, '+wScroll/6+'%)'
+    })
+    // jumbotron
     $('.jumbotron img').css({
-        'transform': 'translate(0px, '+ wScroll/3 +'%)'
+        'transform': 'translate(0px, '+ wScroll/7 +'%)'
     });
     $('.jumbotron h1').css({
         'transform': 'translate(0px, '+wScroll/3.9+'%)'
     });
     $('.jumbotron p').css({
-        'transform': 'translate(0px, '+wScroll/2+'%)'
+        'transform': 'translate(0px, '+wScroll/2.5+'%)'
     });
+
+
+    // portfolio
+    let portfolio = $('.portfolio').offset().top;
+    let arrThumbnail = $('.portfolio .thumbnail');
+    if(wScroll> portfolio-470) {
+        arrThumbnail.each(function(i) {
+            setTimeout(function() {
+                $('.portfolio .thumbnail').eq(i).addClass('appear');
+            },300*(i+1))
+        })
+        // 
+    }
+    else if(wScroll<portfolio-600) {
+        $('.portfolio .thumbnail').removeClass('appear');
+    }
+    
 });
